b2afc06a3cbee229bbea32c52201aeeef3ba9098
Santiago Sainz Fernández <evelpialight@gmail.com>
2014-11-17 17:10:57 +0000

    Añadiendo más pruebas
    Hay que revisarlas

--- spec/spec_test.rb
+++ spec/spec_test.rb
@@ -7,6 +7,15 @@ Coveralls.wear!
 describe 'make API call to load path' do
    it "should load the home page" do
       visit 'http://localhost:4567'
-      expect(page).to have_content("welcome")
+      expect(page).to have_content("Chat")
+   end
+end
+
+describe 'login page' do
+   it 'lets the user login' do
+      visit 'http://localhost:4567'
+      fill_in 'name', :with => 'Hikoreko'
+      click_on 'submit'
+      page.should have_content('welcome')
    end
 end
