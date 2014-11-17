249c4916dd9c39cc141fc313e3ebf8a748554de0
Santiago Sainz Fernández <evelpialight@gmail.com>
2014-11-17 19:15:16 +0000

    Test corregidos
    Solo falta uno, el de siempre xD

--- spec/spec_test.rb
+++ spec/spec_test.rb
@@ -36,7 +36,7 @@ end
 describe 'chat page' do
    it 'have a chat with the chat' do
       visit 'http://localhost:4567/chat'
-      page.fill_in '#text', :with => 'Hello!!'
+      fill_in 'text', :with => 'Hello!!'
       click_on('Enviar')
       expect(page).to have_content("Hello!!")
    end
